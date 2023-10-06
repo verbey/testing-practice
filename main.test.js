7ccc39283adc56be40ae20d4a9344365e965a85a
Verbiturum <user@example.com>
2023-10-06 22:11:18 +0300

    Add tests for the reverse function

--- main.test.js
+++ main.test.js
@@ -1,5 +1,13 @@
-import { capitalize } from "./main";
+import { capitalize, reverse } from "./main";
 
 test("Capitalization test", () => {
 	expect(capitalize("hello")).toBe("Hello");
+});
+
+test("Reverse function test impostor", () => {
+	expect(reverse("impostor")).toBe("rotsopmi");
+});
+
+test("Reverse function test hello", () => {
+	expect(reverse("hello")).toBe("olleh");
 });
