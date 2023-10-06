cd35990f3dae245dd08f2435423683bd450df2bf
Verbiturum <user@example.com>
2023-10-06 22:50:19 +0300

    Write test for Array analyzer function

--- main.test.js
+++ main.test.js
@@ -27,3 +27,12 @@ test("Ceasar cipher", () => {
 	expect(ceasarEncoder("a,a", 1)).toBe("b,b");
 	expect(ceasarEncoder("ZZZ", 1)).toBe("AAA");
 });
+
+test("Array analyzer", () => {
+	expect(analyzeArray([1, 8, 3, 4, 2, 6]).toEqual({
+		average: 4,
+		min: 1,
+		max: 8,
+		length: 6
+	}));
+});
